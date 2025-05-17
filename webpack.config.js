10f4c7a9e39271db1e554881d53cedf7fb309a8e
Ali <dubzaev98@mail.ru>
2025-05-17 17:20:18 +0300

    ww

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/tic-tac-toe/',
   },
   plugins: [
     new HtmlWebpackPlugin({
